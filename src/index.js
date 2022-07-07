bbd7be5cacf0f1a9240416b18d3f43b77d411e8e
OJeanMaciel <jeansu@hotmail.com>
2022-07-07 20:33:37 -0300

    apis

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Body from './Pages/body';
 import Header from './Pages/headers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <Body />
   </React.StrictMode>
 );
