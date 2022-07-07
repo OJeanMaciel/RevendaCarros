931524a24d35e45af2a287a6b0984fba4e9f3f67
OJeanMaciel <jeansu@hotmail.com>
2022-07-07 20:12:11 -0300

    ajuste api

---
+++ src/Pages/carros.js
@@ -0,0 +1,25 @@
+import config from '../config';
+
+const carros = `${config.URL_BACKEND}/carros`;
+
+function create(objetoDoCarro) {
+  return fetch(`${carros}?_embed=carros`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(objetoDoCarro),
+    })
+    .then(async (respostaServidor) => {
+    if (respostaServidor.ok) {
+      const resposta = await respostaServidor.json();
+      return resposta;
+    }
+    throw new Error('Não foi possível pegar os dados');
+  });
+}
+
+export default {
+  create,
+};
+    
