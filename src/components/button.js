7ae5806b3eee14f0c3e43eaffe0f0c9fae800711
OJeanMaciel <jeansu@hotmail.com>
2022-07-11 18:22:20 -0300

    preparação final

---
+++ src/components/button.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+//import { Link } from 'react-router-dom';
+
+const Button = styled.button`
+padding: .7em;
+border-radius: 8px;
+background-color: gray;
+color: white;
+
+
+background-color: rgb(192, 10, 10);
+transition: 0.3s;
+`;
+
+export default Button 
--- src/pages/form.js
+++ src/pages/form.js
@@ -1,14 +1,108 @@
 import React from 'react';
 import '../css/form.css';
-import BodyForm from '../components/body_form';
 import Headers from '../components/headers';
+import carrosRepository from '../repositories/carros';
+import Button from '../components/button';
+import FormField from '../components/FormField';
+import useForm from '../hooks/useForm';
+import { useNavigate  } from 'react-router-dom';
 
 function Form(){
-    return (
+    const navigate = useNavigate ();    
+    const { handleChange, values } = useForm({
+    });
+      return (
         <div>
             <Headers />
-            <BodyForm />
-        </div>
-    )
-}
+
+          <h1>Cadastro de carros</h1>
+  
+          <form onSubmit={(event) => {
+            event.preventDefault();
+            alert('Carro Cadastrado com Sucesso!');
+  
+            carrosRepository.create({
+              marca_nome: values.marca_nome,
+              nome_modelo: values.nome_modelo,
+              ano: values.ano,
+              combustivel: values.combustivel,
+              num_portas: values.num_portas,
+              valor_fipe: values.valor_fipe,
+              cor: values.cor,
+              timestamp_cadastro: values.timestamp_cadastro,
+              id: 1,
+              marca_id: 1,
+            })
+              .then(() => {
+                console.log('Cadastrou com Sucesso!');             
+                navigate.push('/');
+              });
+          }}
+          >
+          <FormField
+            label="Marca do veículo"
+            name="marca_nome"
+            value={values.marca_nome}
+            onChange={handleChange}
+           />
+  
+          <FormField
+            label="Nome do modelo"
+            name="nome_modelo"
+            value={values.nome_modelo}
+            onChange={handleChange}
+          />
+  
+          <FormField
+            label="Ano de Fabricação"
+            name="ano"
+            value={values.ano}
+            onChange={handleChange}
+          />
+  
+          <FormField
+            label="Tipo de combustivel"
+            name="combustivel"
+            value={values.combustivel}
+            onChange={handleChange}
+          />
+  
+          <FormField
+            label="Quantidade de portas"
+            name="num_portas"
+            value={values.num_portas}
+            onChange={handleChange}
+          />
+  
+          <FormField
+            label="Valor da Fipe"
+            name="valor_fipe"
+            value={values.valor_fipe}
+            onChange={handleChange}
+          />
+  
+          <FormField
+            label="Cor do veículo"
+            name="cor"
+            value={values.cor}
+            onChange={handleChange}
+          />
+  
+          <FormField
+            label="Dia do cadastro"
+            name="timestamp_cadastro"
+            value={values.timestamp_cadastro}
+            onChange={handleChange}
+          />
+  
+          <Button type="submit">
+            Cadastrar
+          </Button>
+  
+  
+          </form>
+            </div>
+      );
+    }
+  
 export default Form;
